e1dde87030d1db45bc6b121b040ac19eaf561106
Mike Dean <mikedean50@googlemail.com>
2018-09-22 13:34:37 +0100

    Added constructors to MathEquation Class

--- src/com/pluralsight/calcengine/MathEquation.java
+++ src/com/pluralsight/calcengine/MathEquation.java
@@ -2,10 +2,51 @@ package com.pluralsight.calcengine;
 
 public class MathEquation {
 
-	public double leftVal;
-	public double rightVal;
-	public char opCode;
-	public double result;
+	private double leftVal;
+	private double rightVal;
+	private char opCode;
+	private double result;
+
+	public MathEquation() {
+	}
+
+	public MathEquation(char opCode) {
+		this.opCode = opCode;
+	}
+
+	public MathEquation(char opCode, double leftVal, double rightVal) {
+		this(opCode);
+		this.leftVal = leftVal;
+		this.rightVal = rightVal;
+	}
+
+	public double getLeftVal() {
+		return leftVal;
+	}
+
+	public void setLeftVal(double leftVal) {
+		this.leftVal = leftVal;
+	}
+
+	public double getRightVal() {
+		return rightVal;
+	}
+
+	public void setRightVal(double rightVal) {
+		this.rightVal = rightVal;
+	}
+
+	public char getOpCode() {
+		return opCode;
+	}
+
+	public void setOpCode(char opCode) {
+		this.opCode = opCode;
+	}
+
+	public double getResult() {
+		return result;
+	}
 
 	public void execute() {
 		switch (opCode) {
@@ -27,6 +68,4 @@ public class MathEquation {
 				break;
 		}
 	}
-
-
 }
